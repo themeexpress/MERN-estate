d0495b2fef19edfc2a5ac1ef1608c3ac6de80044
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2024-04-05 02:55:34 +0900

    Added Route for necessary pages
    Used BrowserRouter, Routes, and Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+// import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/Signin'
+import Profile from './pages/Profile'
 
 export default function App() {
-  return (
-    <div className='text-red-500'>App</div>
-  )
+  return <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>;
 }
