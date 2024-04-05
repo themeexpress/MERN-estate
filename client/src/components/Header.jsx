1e91f5c4985378a598311eaf0bf42aab462796ab
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2024-04-05 23:58:57 +0900

    Added Backend API and install express
    Install express and run the server via nodemon
    Added react-icons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FaSearch} from "react-icons/fa"
 import { Link } from 'react-router-dom'
 
 export default function Header() {
@@ -13,8 +14,9 @@ export default function Header() {
             </Link>
             <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
                 <input type='text' placeholder='Search...' 
-                    className='bg-transparent focus:outline-none w-24 sm:w-64'
-                ></input>
+                    className='bg-transparent focus:outline-none w-24 sm:w-64'>
+                </input>
+                <FaSearch className="text-slate-600"/>
             </form>
             <ul className='flex gap-4'>
                 <li className='hidden sm:inline text-slate-700 hover:underline'><Link to="/">Home</Link></li>
