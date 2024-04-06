8f8d1a180fcdb4da7bd7cde49758b5faeffc99bb
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2024-04-07 01:16:23 +0900

    Created test API router, Controller and Route Proxy

--- api/index.js
+++ api/index.js
@@ -1,18 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from './routes/user.route.js'
 import dotenv from 'dotenv';
 
+// init dotenv
 dotenv.config()
 
+//connect the MongoDB
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('connect to MangoDB');
     }).catch((err) =>{
         console.log(err)
     })
 
-
 const app = express();
 
 app.listen(3000, () => {
     console.log("server is running on port 3000!!!")
 })
+
+// test route
+app.use('/api/user', userRouter);
