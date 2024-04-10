a82ce9828dbc452ca57e4b210eac2c76885ae428
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2024-04-10 23:47:21 +0900

    Added Redux toolkit and used in signInpage
    redux tool kit, update the error handling and state handling by redux toolkit.

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
-import { Link, useNavigate} from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useNavigate} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false)
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -13,11 +15,12 @@ export default function SignIn() {
       [e.target.id]: e.target.value,
     });
   };
-  //console.log(formData);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try{
-      setLoading(true)
+      // setLoading(true)
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', 
       {
         method: 'POST',
@@ -29,17 +32,14 @@ export default function SignIn() {
       const data = await res.json();
       console.log(data);
       if (data.success == false){
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null)
+      dispatch(signInSuccess(data));
       // There is no error and user is created so navigate the sign-in page
       navigate('/');
     }catch(error){
-      setLoading(false)
-      setError(error.message)
+      dispatch(signInFailure(error.message));
     }
   }
   return (
