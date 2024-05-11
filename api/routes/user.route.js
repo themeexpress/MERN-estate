06e792fd9224ecc5fb98d437cc59d896dce2ce54
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2024-05-11 23:22:15 +0900

    Created get listing for user API Route
    Completed get listing API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {test, updateUser, deleteUser} from '../controllers/user.controller.js'
+import {test, updateUser, deleteUser, getUserListing} from '../controllers/user.controller.js'
 import { verifyToken } from "../utils/verifyUser.js";
 import { signOut } from "../controllers/auth.controller.js";
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
-router.get('/signout', signOut)
+router.get('/signout', signOut);
+router.get('/listings/:id', verifyToken, getUserListing)
 
 export default router;
