9a05fe3f5542681833e5b0289d677e5dcdda2de4
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2024-05-12 22:00:26 +0900

    completed update listing api route
    Update listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -10,7 +10,6 @@ export const createListing = async (req, res, next) => {
     }
 }
 
-
 export const deleteListing = async (req, res, next) => {
     const listing = await Listing.findById(req.params.id);
     if(!listing){
@@ -25,4 +24,25 @@ export const deleteListing = async (req, res, next) => {
     } catch (error) {
         next(error)
     }
+}
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if(!listing){
+        next(errorHandler(404, 'Listing not found'));
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401, 'You can only update your own listing'));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+    } catch (error) {
+        next(error);
+    }
+
 }
