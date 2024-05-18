dc4299673f91e216a198ec7058e064f1d7d3107d
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2024-05-18 16:06:34 +0900

    Completed search UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,13 +10,15 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing  from './pages/UpdateListing';
 import { Listing } from './pages/Listing';
+import Search from './pages/Search';
 
 export default function App() {
   return <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='search' element={<Search/>}></Route> 
         <Route path="/listing/:listingId" element={<Listing />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
